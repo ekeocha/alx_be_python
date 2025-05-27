c2a20d4bbe2170233c32966bcff8fa412e0e57d5
ekeocha <ekeochacr@gmail.com>
2025-05-27 17:12:32 +0100

    added task 0

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,12 @@
+# python arithematic task ALX, define a variable of 2 numbers and perform arithematic operations and print results
+
+number1 = 10
+number2 = 5
+
+sum = number1 + number2
+difference = number2 - number1
+product = number1 * number2
+
+print("The sum of number1 and number2 is ", sum)
+print("the difference of number1 and number2 is ", difference)
+print("the product of number1 and number2 is ",  product )
