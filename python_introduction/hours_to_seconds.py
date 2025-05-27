ad581312d322974ff9e8f5609e5575a08bef304a
ekeocha <ekeochacr@gmail.com>
2025-05-27 18:25:32 +0100

    made indentation

--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -1,6 +1,7 @@
 #write a Python script that converts a specific number of hours into seconds. This task reinforces the concept of arithmetic operations within a practical context.
 
 hours = 2 
+
 seconds= hours * 3600
 
 print(hours,"hours is",seconds, "seconds")
