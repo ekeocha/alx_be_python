51a949ba9c272aabb9a4a9318f8da2bd541b93c1
ekeocha <ekeochacr@gmail.com>
2025-05-27 17:31:11 +0100

    add simple_interest

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,13 @@
+#Your task is to complete a Python script that calculates the simple interest earned on an investment over a period of time. The formula for simple interest is (I = P * R * T), where:
+#( I ) is the interest earned,
+#( P ) is the principal amount (initial investment),
+#( R ) is the annual interest rate (as a decimal),
+#( T ) is the time the money is invested for in years.
+
+principal = 1000
+rate  =  0.05
+time = 3 
+
+Interest = float(principal*rate*time)
+
+print("the simple interst is ", Interest)
