692c423d671a3f9b1927a3009fdf11d80bf261f8
ekeocha <ekeochacr@gmail.com>
2025-06-01 22:51:40 +0100

    indentation work

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,13 +1,12 @@
 #Weather advice app
+weather = input("What's the weather like today? (sunny/rainy/cold): ").lower()
 
-weather = input("What's the weather like today? (sunny/rainy/cold): ")
-
+# Make clothing recommendations based on the weather
 if weather == "sunny":
-    print("Wear a t-shirt and sunglasses")
+    print("Wear a t-shirt and sunglasses.")
 elif weather == "rainy":
-    print("Don't forget your umbrella and a raincoat")
+    print("Don't forget your umbrella and a raincoat.")
 elif weather == "cold":
     print("Make sure to wear a warm coat and a scarf.")
-
 else:
     print("Sorry, I don't have recommendations for this weather.")
