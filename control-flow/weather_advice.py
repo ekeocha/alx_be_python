602817e09103564a61ba99c3508ec2b5f9a37ca4
ekeocha <ekeochacr@gmail.com>
2025-06-01 22:49:53 +0100

    changed variable name

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,12 +1,12 @@
 #Weather advice app
 
-user_weather = input("What's the weather like today? (sunny/rainy/cold): ")
+weather = input("What's the weather like today? (sunny/rainy/cold): ")
 
-if user_weather == "sunny":
+if weather == "sunny":
     print("Wear a t-shirt and sunglasses")
-elif user_weather == "rainy":
+elif weather == "rainy":
     print("Don't forget your umbrella and a raincoat")
-elif user_weather == "cold":
+elif weather == "cold":
     print("Make sure to wear a warm coat and a scarf.")
 
 else:
