0888528824f8fa67e6b63184861bd93ecc03d8ae
ekeocha <ekeochacr@gmail.com>
2025-06-08 22:22:59 +0100

    Added arithmetic_operations

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,32 +1,7 @@
-task = input("Enter your task: ")
-priority = input("Priority (high/medium/low): ").lower()
+def cube(side):
+  volume = side **3
+  surface_area = 6 * (side**2)
+  return volume, surface_area
 
-time_bound = input("Is it time-bound? (yes/no): ").lower()
-
-yesno_list =["yes", "no"]
-
-if time_bound not in yesno_list:
-	print(f"Incorrect time_bound value -{time_bound}")
-	
-else:
-	match priority:
-		case "high":
-			if time_bound == "yes":
-				print(f"Reminder: '{task}' is a {priority} priority task that requires immediate attention today!")
-			else:
-				print(f"Note: '{task}' is a {priority} priority task. Consider completing it when you have free time.")	
-  
-		case "medium":
-			if time_bound == "yes":
-				print(f"Reminder: '{task}' is a {priority} priority task that requires your attention!")
-			else:
-				print(f"Note: '{task}' is a {priority} priority task. Consider completing it when you have free time.")
-            
-		case "low":
-			if time_bound == "yes":
-				print(f"Reminder: '{task}' is a {priority} priority task that requires your attention!")
-			else:
-				print(f"Note: '{task}' is a {priority} priority task. Consider completing it when you have free time.")
-	        	
-		case _ :
-			print(f"Incorrect priority value -{priority}")
+result = cube(3)
+print(f"{result}")
