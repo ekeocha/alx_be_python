65ae5b0a0ec23468dba64f1c8616a94d17d3258c
ekeocha <ekeochacr@gmail.com>
2025-07-05 08:47:45 +0100

    bank_acount

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,16 @@
+class BankAccount():
+    
+    def __init__(self,account_balance):
+        self.account_balance = account_balance
+
+    def deposit(self,amount):
+        if amount > 0:
+            self.account_balance += amount
+
+    def withdraw(self, amount):
+        if 0 < amount <= self.account_balance:
+            return True
+        return False
+    
+    def display_balance(self):
+        print(f"Your account balance is {self.account_balance:.2f}") 
